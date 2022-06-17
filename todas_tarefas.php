2f471242195185c9e86f085813e04115b159270d
litoralcar <criacao1@litoralcar.com.br>
2022-06-17 17:16:49 -0300

    salva private

--- todas_tarefas.php
+++ todas_tarefas.php
@@ -49,7 +49,7 @@ require 'tarefa_controller.php';
 							?>
 
 								<div class="row mb-3 d-flex align-items-center tarefa">
-									<div class="col-sm-9"><?= $tarefa->tarefa ?> (<?= $tarefa->id_status ?>)</div>
+									<div class="col-sm-9"><?= $tarefa->tarefa ?> (<?= $tarefa->status ?>)</div>
 									<div class="col-sm-3 mt-2 d-flex justify-content-between">
 										<i class="fas fa-trash-alt fa-lg text-danger"></i>
 										<i class="fas fa-edit fa-lg text-info"></i>
